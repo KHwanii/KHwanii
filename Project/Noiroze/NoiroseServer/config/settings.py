64a4514dc3bf0d2971d2b12f46c5a0370b3b6fe7
unknown <digitwk@gmail.com>
2023-07-26 22:35:33 +0900

    로그인페이지로 리다이렉트 하는 기능 추가

--- Project/Noiroze/NoiroseServer/config/settings.py
+++ Project/Noiroze/NoiroseServer/config/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
+'''
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
@@ -103,7 +103,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-'''
+
 
 
 # Password validation
--- Project/Noiroze/NoiroseServer/config/urls.py
+++ Project/Noiroze/NoiroseServer/config/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from common.views import redirect_login_page
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('common/', include('common.urls')),               # common 에서 설정한 urls 앞에 common/ 이 추가됨
     path('main/', include('main.urls')),                   # main 에서 설정한 urls 앞에 main/ 이 추가됨
     path('api/', include('api.urls')),                     # api 에서 설정한 urls 앞에 api/ 이 추가됨
-
+    path('', redirect_login_page)                          # common/views.py 에서 만든 redirect 함수를 통해, 기본 페이지로 이동 시, 로그인 페이지로 이동하도록 설정
  
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
